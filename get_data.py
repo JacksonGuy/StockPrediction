f152d8d931a9812b9d51927dc1c135510d8af848
Jackson Frey <jacksonf955@gmail.com>
2024-02-13 23:25:30 -0500

    forex comparison

---
+++ get_data.py
@@ -0,0 +1,51 @@
+import yfinance as yf
+from time import sleep
+
+# Top 20 world currencies
+currencyCodes = [
+    'EUR',      # Euro
+    'JPY',      # Japan
+    'GBP',      # UK 
+    'CNY',      # China
+    'AUD',      # Australia
+    'CAD',      # Canada
+    'CHF',      # Swiss
+    'HKD',      # Hong Kong
+    'SGD',      # Singapore
+    'SEK',      # Swedian
+    'KRW',      # South Korea
+    'NOK',      # Norway
+    'NZD',      # New Zealand
+    'INR',      # India
+    'MXN',      # Mexico
+    'TWD',      # Taiwan
+    'ZAR',      # South Africa
+    'BRL',      # Brazil
+    'DKK'       # Denmark
+]
+
+changes = {}
+
+# Get initial prices
+for code in currencyCodes:
+    data = yf.Ticker("USD" + code + "=X")
+    price = data.info['bid']
+
+    changes["USD" + code] = price
+    
+    #print("USD/" + code)
+    #print(price)
+    #print()
+
+sleep(60)
+for i in range(10):
+    print("Minute " + str(i))
+    for code in currencyCodes:
+        data = yf.Ticker("USD" + code + "=X")
+        price = data.info['bid']
+
+        if (price > changes["USD" + code]):
+            print("Price increase for USD/" + code)
+
+    print()
+    sleep(60)
