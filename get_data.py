fa24f620e963ec7c25d08e04cad8ac6b2dfd119c
JacksonGuy <jacksonf955@gmail.com>
2024-02-24 12:12:51 -0500

    small changes

--- get_data.py
+++ get_data.py
@@ -1,12 +1,23 @@
 import yfinance as yf
+from datetime import datetime
 
 import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-def get_hist_data(ticker, period, interval):
+def get_hist_data(ticker, period="5d", interval="1m"):
     curr = yf.Ticker(ticker)
-    hist = curr.history(period=period, interval=interval)
+    dates = get_date_range()
+    hist = curr.history(period=period, interval=interval, 
+                        start = dates["start"], end = dates["end"])
     return hist
+
+def get_date_range():
+    today = datetime.today()
+    start = today.day - 8
+    end = today.day - 1
+    startDate = "{}-{}-{}".format(today.year, today.month, start)
+    endDate = "{}-{}-{}".format(today.year, today.month, end)
+    return {"start" : startDate, "end" : endDate}
 
 def get_current_exchange(ticker):
     data = yf.Ticker(ticker)
